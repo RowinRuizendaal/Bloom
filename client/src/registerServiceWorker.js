0140b75466c0b40ad46512418ffc8b63f29415b7
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-05-18 18:52:57 +0200

    [feature: indexpage][setup: linting]

--- client/src/registerServiceWorker.js
+++ client/src/registerServiceWorker.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-console */
 
-import { register } from 'register-service-worker';
+import {register} from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
       console.log(
-        'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB',
+        "App is being served from cache by a service worker.\n" +
+          'For more details, visit https://goo.gl/AFskqB'
       );
     },
     registered() {
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is available; please refresh.');
     },
     offline() {
+      // eslint-disable-next-line max-len
       console.log('No internet connection found. App is running in offline mode.');
     },
     error(error) {
