0080963ad49076759d3549b443d129c78f4f13c3
ralfz123 <ralf_zonneveld@hotmail.com>
2021-05-18 19:05:37 +0200

    setup begin

---
+++ client/src/views/login/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="register">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Registerrrrrrrrr"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
