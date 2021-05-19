7ecf1bb8e036387f754d7ddef8fbb796cb532052
ralfz123 <ralf_zonneveld@hotmail.com>
2021-05-19 17:17:53 +0200

    solved issue

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,40 +1,40 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/register',
-    name: 'Register',
+    path: "/register",
+    name: "Register"
     // component: Register
-    component: () => import(/* webpackChunkName: "about" */ '../views/login/Login.vue'),
+    // component: () => import(/* webpackChunkName: "about" */ '../views/login/Login.vue'),
   },
   {
-    path: '/login',
-    name: 'Login',
-    component: Login,
+    path: "/login",
+    name: "Login"
+    // component: Login,
   },
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: "/",
+    name: "Home",
+    component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+    component: () => import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }
 ];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes,
+  routes
 });
 
 export default router;
