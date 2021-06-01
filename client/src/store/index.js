ad7ca75c0f8d9e0418effc9482f58423b04c9160
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-06-01 18:39:49 +0200

    [refactor: themes]

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -20,6 +20,7 @@ export default new Vuex.Store({
           "Bijvoorbeeld afwassen of stofzuigen. Je bent elke dag moe. De vermoeidheid gaat niet over. Ook niet als je even gaat zitten en uitrust.",
         ],
         quote: `"Ik mezelf behoorlijk uitgeput. Dit zorgt voor vermoeidheid zowel lichamelijk als mentaal"`,
+        klachtenheader: "Andere klachten",
         klachten1:
           "Als je erg moe bent, heb je weinig energie en kun je weinig doen. Dat heeft invloed op je emoties en gevoelens. Je bent bijvoorbeeld snel boos of somber. Mensen begrijpen vaak niet hoe het is om steeds moe te zijn. Dat kan je eenzaam maken.",
         klachten2:
@@ -58,9 +59,10 @@ export default new Vuex.Store({
         ],
       },
       {
+        // Klachten
         id: 2,
         name: "Angst",
-        image: "1.jpg",
+        image: "2.jpg",
         intro:
           "Een ernstige ziekte kan je angstig maken. Dat is begrijpelijk. Je leven en toekomstplannen zijn ingrijpend veranderd. Veel mensen met kanker zijn angstig. Dat is een normale reactie. Ben je vaak angstig en gaat dat gevoel niet over? Dan is het belangrijk dat je hulp zoekt.",
         p1: "",
@@ -73,12 +75,47 @@ export default new Vuex.Store({
           "Je kunt je niet goed concentreren.",
           "Je hebt last van hartkloppingen, je trilt en bent vaak duizelig.",
         ],
-        quote: `“Tijdens het behandelingstraject zit je heel erg vast in je angsten. Nu komt dat er langzaam uit.”`,
+        quote: `“Tijdens het behandelingstraject zit je heel erg vast in je angsten. Nu komt dat er langzaam uit.” `,
+        klachtenheader: "",
+        klachten1: "",
+        klachten2: "",
+
+        // Tips
+        heading2: "Tips om je minder angstig te voelen",
+        tipslijst: [
+          "Praat met iemand over je angst en je gevoelens.",
+          "Doe dingen die je leuk vindt en waar je van ontspant.",
+          "Zoek afleiding. Plan bijvoorbeeld een uitje. Dan heb je iets om naar uit te kijken.",
+          "Doe oefeningen om te ontspannen en rustig te ademen. Bijvoorbeeld door yoga of mindfulness te doen.",
+        ],
+
+        // Hulp
+        heading3: "Hulp bij angst",
+        p2: "Voel je je vaak angstig? Bespreek dat dan met je huisarts. Of in het ziekenhuis met de verpleegkundige of arts. Vaak lucht het op om over je angst te praten. En je kan tips krijgen om je angst te verminderen. Je huisarts of arts kan je doorverwijzen naar een therapeut. Zo’n zorgverlener kan je helpen om beter met je angst om te gaan. Bijvoorbeeld door met je te praten. Of met een therapie.Je kunt ook over je angst praten met lotgenoten. Die ontmoet je in een inloophuis of bij een patiëntenorganisatie.Uit je je gevoelens liever op een andere manier, bijvoorbeeld in schilderen, dans of muziek? Dan kun je terecht bij een vaktherapeut.",
+        links: [
+          {
+            website: "Haaglanden Medisch Centrum",
+            url: "https://google.com",
+          },
+          {
+            website: "Kenniscentrum Chronische Vermoeidheid",
+            url: "https://google.com",
+          },
+          {
+            website: "Psychologenpraktijk M. Kalogridi",
+            url: "https://google.com",
+          },
+          {
+            website: "Eerstelijnspsychologiepraktijk Vosmeer",
+            url: "https://google.com",
+          },
+        ],
       },
       {
+        // Klachten
         id: 3,
         name: "Somberheid",
-        image: "1.jpg",
+        image: "3.jpg",
         intro:
           "Een ernstige ziekte kan je somber maken. Dat is heel begrijpelijk. Je dagelijks leven en je toekomstplannen zijn ingrijpend veranderd. Ben je vaak somber en gaat dat gevoel niet over? Dan is het belangrijk dat je hulp zoekt.",
         p1: "",
@@ -91,11 +128,50 @@ export default new Vuex.Store({
           "Niemand kan je opvrolijken. Ook je vrienden of partner niet.",
         ],
         quote: `“Ik zeg tegen mijn vrienden dat het goed met me gaat. Maar ik voel me vaak eenzaam en somber.”`,
+        klachtenheader: "",
+        klachten1: "",
+        klachten2: "",
+
+        // Tips
+        heading2: "Tips om je minder somber te voelen",
+        tipslijst: [
+          "Praat met iemand over je sombere gevoelens.",
+          "Denk niet steeds aan de toekomst. Probeer vooral in het hier en nu te leven.",
+          "Zeg hardop ‘stop’ tegen jezelf als je merkt dat je piekert.",
+          "Zorg voor voldoende slaap.",
+          "Doe dingen die je leuk vindt en waar je van ontspant.",
+          "Doe oefeningen om te ontspannen en rustig te ademen. Bijvoorbeeld door yoga of mindfulness te doen.",
+          "Breng structuur aan in je dag. Probeer op vaste tijden op te staan en te gaan slapen. Eet gezond en op dezelfde tijden.",
+          "Onderneem iets. Plan bijvoorbeeld een uitje. Dan heb je iets om naar uit te kijken. Je hebt er waarschijnlijk geen zin in. Maar je bent wel even afgeleid van je sombere gedachten.",
+        ],
+
+        // Hulp
+        heading3: "Hulp bij somberheid",
+        p2: "Al weken lang somber? Ga dan naar je huisarts. Je huisarts kan bijvoorbeeld medicijnen voorschrijven. Die helpen om rustiger te worden. De huisarts kan je ook doorverwijzen naar een gespreksgroep. In een gespreksgroep praat je met mensen die hetzelfde voelen als jij. Je huisarts kan je tips geven over verschillende trainingen en activiteiten. Die helpen je om minder te piekeren. Vind je praten lastig? Of uit je je gevoelens liever op een andere manier? Bijvoorbeeld door te schilderen, of aan muziek of beweging te doen. Dan kun je terecht bij een vaktherapeut.",
+        links: [
+          {
+            website: "Haaglanden Medisch Centrum",
+            url: "https://google.com",
+          },
+          {
+            website: "Kenniscentrum Chronische Vermoeidheid",
+            url: "https://google.com",
+          },
+          {
+            website: "Psychologenpraktijk M. Kalogridi",
+            url: "https://google.com",
+          },
+          {
+            website: "Eerstelijnspsychologiepraktijk Vosmeer",
+            url: "https://google.com",
+          },
+        ],
       },
       {
+        // Klachten
         id: 4,
         name: "Mijn relatie",
-        image: "1.jpg",
+        image: "4.jpg",
         intro:
           "Kanker zet je leven op z’n kop. Niet alleen je eigen leven. Ook het leven van je partner. Jullie moeten allebei wennen aan de nieuwe situatie. En jullie moeten allebei de gebeurtenissen en emoties verwerken. Hierdoor kunnen relatieproblemen ontstaan. Praat er samen over. En zoek hulp als jullie er samen niet uitkomen.",
         p1: "",
@@ -109,12 +185,96 @@ export default new Vuex.Store({
           "Je vindt het moeilijk om met je partner over seks en intimiteit te praten.",
           "Je bent bang dat je partner je verlaat omdat je ziek bent.",
         ],
-        quote:
-          "“Wij hebben geen standaard relatieproblemen, maar we hebben kanker tussen ons instaan.”",
+        quote: `“Wij hebben geen standaard relatieproblemen, maar we hebben kanker tussen ons instaan.”`,
+        klachtenheader: "Alles is anders",
+        klachten1:
+          "Kanker, de behandelingen en alle veranderingen moet je verwerken. Iedereen doet dat op z’n eigen manier. Je partner wil er misschien elke dag over praten en jij niet. Of omgekeerd. Hierdoor kun je relatieproblemen krijgen. Die problemen kunnen je extra onzeker of somber maken. Weet dat je niet de enige bent.",
+        klachten2: "",
+
+        // Tips
+        heading2: "Tips om relatieproblemen te voorkomen",
+        tipslijst: [
+          "Praat met iemand over je sombere gevoelens.",
+          "Praat erover en doe dat zo open en eerlijk mogelijk. Zo zorg je er samen voor dat jullie geen relatieproblemen krijgen. Of dat die worden opgelost.",
+          "Sommige dingen wil je misschien niet met je partner bespreken. Of het lukt niet om er met je partner over te praten. Zoek dan steun bij iemand anders. Bijvoorbeeld bij een goede vriend of vriendin. Bij je huisarts. Of bij een geestelijk verzorger, maatschappelijk werker of therapeut.",
+        ],
+
+        // Hulp
+        heading3: "Hulp je kinderen betrekken?",
+        p2: "Kanker heeft ook invloed op het leven van je kind of kinderen. Hoe ga je daarmee om?",
+        links: [
+          {
+            website: "Haaglanden Medisch Centrum",
+            url: "https://google.com",
+          },
+          {
+            website: "Kenniscentrum Chronische Vermoeidheid",
+            url: "https://google.com",
+          },
+          {
+            website: "Psychologenpraktijk M. Kalogridi",
+            url: "https://google.com",
+          },
+          {
+            website: "Eerstelijnspsychologiepraktijk Vosmeer",
+            url: "https://google.com",
+          },
+        ],
       },
       {
+        // Klachten
         id: 5,
-        name: "Geldzaken & werk",
+        name: "Geld en werk",
+        image: "5.jpg",
+        intro:
+          "Door een ernstige ziekte ontstaan soms geldproblemen. Bijvoorbeeld door extra kosten. Of doordat je minder of helemaal niet kunt werken door je ziekte. Heb je geldproblemen of schulden? Praat erover en zoek hulp.",
+        p1: "Geldzorgen kunnen je somber, onzeker of angstig maken. Ook kun je er slecht door gaan slapen. Weet dat je niet de enige bent. Veel mensen met kanker hebben geldproblemen. Vaak schamen ze zich. Ze praten er daarom liever niet over. Dat maakt de geldzorgen en negatieve gevoelens vaak alleen maar erger.",
+        klachtenlijst: [
+          "Je hebt geldzorgen en je piekert veel.",
+          "Je weet niet hoe je alle rekeningen moet betalen.",
+          "Je schaamt je voor je geldproblemen. Daarom praat je er met niemand over. Dat geeft je een eenzaam gevoel.",
+          "Je slaapt niet goed door de geldzorgen.",
+          "Je maakt de post niet meer open. Je bent bang dat het rekeningen zijn.",
+          "Je bent bang dat je dingen moet verkopen. Bijvoorbeeld je auto of je huis.",
+        ],
+        quote: `“Ik vond het moeilijk om op werk over m’n klachten te praten. Het gesprek met mijn teamleider hielp.”`,
+        klachtenheader: "",
+        klachten1: "",
+        klachten2: "",
+
+        // Tips
+        heading2: "Tips om geldzorgen te verminderen",
+        tipslijst: [
+          "Praat met iemand over je geldzorgen en problemen. Bijvoorbeeld met je huisarts. Die kan je doorverwijzen naar een maatschappelijk werker.",
+          "Je kunt ook praten met je leidinggevende of met een vertrouwenspersoon op je werk. Of je kunt een afspraak maken met een schuldhulpverlener van je gemeente.",
+        ],
+
+        // Hulp
+        heading3: "Hulpaanbieders bij jou in de buurt",
+        p2: "Je kunt bij onderstaande hulpaanbieders terecht met langdurige relatieproblemen.",
+        links: [
+          {
+            website: "Haaglanden Medisch Centrum",
+            url: "https://google.com",
+          },
+          {
+            website: "Kenniscentrum Chronische Vermoeidheid",
+            url: "https://google.com",
+          },
+          {
+            website: "Psychologenpraktijk M. Kalogridi",
+            url: "https://google.com",
+          },
+          {
+            website: "Eerstelijnspsychologiepraktijk Vosmeer",
+            url: "https://google.com",
+          },
+        ],
+      },
+      {
+        // Klachten
+        id: 6,
+        name: "Lichaam & uiterlijk",
         image: "1.jpg",
         intro:
           "Door een ernstige ziekte ontstaan soms geldproblemen. Bijvoorbeeld door extra kosten. Of doordat je minder of helemaal niet kunt werken door je ziekte. Heb je geldproblemen of schulden? Praat erover en zoek hulp.",
@@ -127,10 +287,39 @@ export default new Vuex.Store({
           "Je maakt de post niet meer open. Je bent bang dat het rekeningen zijn.",
           "Je bent bang dat je dingen moet verkopen. Bijvoorbeeld je auto of je huis.",
         ],
-        quote:
-          "“Ik vond het moeilijk om op werk over m’n klachten te praten. Het gesprek met mijn teamleider hielp.”",
+        quote: `“Ik vond het moeilijk om op werk over m’n klachten te praten. Het gesprek met mijn teamleider hielp.”`,
+        klachten1: "",
+        klachten2: "",
+
+        // Tips
+        heading2: "Tips om geldzorgen te verminderen",
+        tipslijst: [
+          "Praat met iemand over je geldzorgen en problemen. Bijvoorbeeld met je huisarts. Die kan je doorverwijzen naar een maatschappelijk werker.",
+          "Je kunt ook praten met je leidinggevende of met een vertrouwenspersoon op je werk. Of je kunt een afspraak maken met een schuldhulpverlener van je gemeente.",
+        ],
+
+        // Hulp
+        heading3: "Hulpaanbieders bij jou in de buurt",
+        p2: "Je kunt bij onderstaande hulpaanbieders terecht met langdurige relatieproblemen.",
+        links: [
+          {
+            website: "Haaglanden Medisch Centrum",
+            url: "https://google.com",
+          },
+          {
+            website: "Kenniscentrum Chronische Vermoeidheid",
+            url: "https://google.com",
+          },
+          {
+            website: "Psychologenpraktijk M. Kalogridi",
+            url: "https://google.com",
+          },
+          {
+            website: "Eerstelijnspsychologiepraktijk Vosmeer",
+            url: "https://google.com",
+          },
+        ],
       },
-      { id: 6, name: "Lichaam & uiterlijk" },
     ],
     // Onboarding states
     activeIndex: 0,
--- client/src/views/themes/slug.vue
+++ client/src/views/themes/slug.vue
@@ -28,7 +28,12 @@
       <div class="text">
         <p class="intro">{{ article.intro }}</p>
         <div class="toggle">
-          <span v-for="(toggle, index) in toggles" :key="index" @click="checkindex(index)">
+          <span
+            v-for="(toggle, index) in toggles"
+            :key="index"
+            @click="checkindex(index)"
+            :class="{ 'active-class': index === data }"
+          >
             <p>{{ toggle.heading }}</p>
           </span>
         </div>
@@ -49,7 +54,7 @@
             {{ article.quote }}
           </blockquote>
 
-          <h2 class="heading">Andere klachten</h2>
+          <h2 class="heading">{{ article.klachtenheader }}</h2>
           <p>{{ article.klachten1 }}</p>
           <p>{{ article.klachten2 }}</p>
         </div>
@@ -93,7 +98,6 @@ export default {
   },
   methods: {
     checkindex(index) {
-      console.log(index);
       this.data = index;
     },
   },
@@ -122,172 +126,5 @@ export default {
 </script>
 
 <style lang="scss">
-.slug {
-  height: 100vh;
-  header {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    nav {
-      display: flex;
-      justify-content: space-evenly;
-      align-items: center;
-      margin-top: 4rem;
-      li {
-        list-style: none;
-        &:nth-child(1) {
-          transform: rotate(180deg);
-          margin-right: 4rem;
-        }
-      }
-      .logo {
-        color: $orange;
-        font-size: 1.167rem;
-        margin: auto;
-        min-width: 8rem;
-      }
-      .skip {
-        font-size: 1rem;
-        color: $gray;
-        text-decoration: none;
-      }
-    }
-  }
-  .toggle {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    border-radius: 5px;
-    align-items: center;
-    text-align: center;
-
-    span {
-      color: $orange;
-      background-color: $yellow;
-      width: 10rem;
-      cursor: pointer;
-      height: 3rem;
-      &:first-child {
-        border-top-left-radius: 5px;
-        border-bottom-left-radius: 5px;
-      }
-      &:last-child {
-        border-top-right-radius: 5px;
-        border-bottom-right-radius: 5px;
-      }
-    }
-  }
-  article {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
-    margin-top: 2rem;
-  }
-  .image {
-    margin-bottom: 1.5rem;
-    max-width: 100%;
-    width: 90%;
-    filter: opacity(90%);
-    border-top-left-radius: 20px;
-    border-top-right-radius: 20px;
-    height: 12rem;
-    object-fit: cover;
-    @include q-lg-min {
-      max-width: 30rem;
-    }
-  }
-  .heading {
-    color: $orange;
-    line-height: 1.5rem;
-    font-size: 1.5rem;
-    margin: 1rem 0rem 1rem 0rem;
-  }
-  .text {
-    line-height: 1.6rem;
-    max-width: 30rem;
-    font-size: 1rem;
-    padding: 20px;
-
-    .intro {
-      color: $gray;
-    }
-
-    p {
-      &:nth-child(1) {
-        font-family: Nunito;
-        font-weight: bold;
-      }
-      font-family: Nunito;
-      font-weight: lighter;
-      margin-top: 1rem;
-      margin-bottom: 1rem;
-    }
-  }
-  .listeditem {
-    list-style: none;
-    font-family: Nunito;
-    font-weight: lighter;
-
-    li {
-      padding: 1rem;
-    }
-    li::before {
-      content: "\2022";
-      color: $orange;
-      font-weight: bold;
-      display: inline-block;
-      width: 1em;
-      margin-left: -1em;
-    }
-  }
-  blockquote {
-    margin-top: 1.5rem;
-    color: $orange;
-    font-family: Nunito;
-    font-weight: bolder;
-    font-size: 1.5rem;
-    position: relative;
-    border-radius: 5px;
-    line-height: 1.6rem;
-  }
-  .header {
-    display: flex;
-    align-items: center;
-    width: 30rem;
-    h1 {
-      margin-left: 1.3rem;
-      @include q-lg-min {
-        margin-left: 0px;
-      }
-    }
-    p {
-      margin-left: auto;
-      margin-right: 1.5rem;
-      color: $orange;
-      @include q-lg-min {
-        margin-right: 0px;
-      }
-    }
-  }
-  .tips,
-  .hulp {
-    .heading {
-      margin-top: 5rem;
-    }
-  }
-  .hulp {
-    .listeditem {
-      li {
-        &:before {
-          content: "";
-        }
-      }
-    }
-    .nextButton {
-      margin-left: 0rem;
-      margin-right: 0rem;
-    }
-  }
-}
+@import "@/components/themes/theme.scss";
 </style>
