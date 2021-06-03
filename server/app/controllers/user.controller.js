32c91f59bf632ccd20cbbeb11996cbf044bcdfcc
ralfz123 <ralf_zonneveld@hotmail.com>
2021-06-03 12:22:37 +0200

    [refactored: names + comments]

--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -59,7 +59,6 @@ async function handleChats(req, res) {
   const userData = await findOneUser(req.params.id);
 
   const chatsData = userData.chats;
-  console.log(chatsData);
 
   // return data
   return res.json(chatsData);
@@ -87,31 +86,3 @@ module.exports = {
   handleChats,
   handleChatParticipants,
 };
-
-// // const db = require('../models/index.js');
-// // const Users = db.users;
-
-// // Create and Save a new Tutorial
-// exports.create = (req, res) => {};
-
-// // Retrieve all Tutorials from the database.
-// exports.findAll = (req, res) => {};
-
-// // Find a single Tutorial with an id
-// exports.findOne = async(email) => {
-//     console.log(email);
-//     // const userData = await Users.findOne({ emailAddress: email });
-//     // return userData};
-// };
-
-// // Update a Tutorial by the id in the request
-// exports.update = (req, res) => {};
-
-// // Delete a Tutorial with the specified id in the request
-// exports.delete = (req, res) => {};
-
-// // Delete all Tutorials from the database.
-// exports.deleteAll = (req, res) => {};
-
-// // Find all published Tutorials
-// exports.findAllPublished = (req, res) => {};
