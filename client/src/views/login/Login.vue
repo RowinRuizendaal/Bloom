7496cf18c271796b51d03719d917d0d7a1317c60
ralfz123 <ralf_zonneveld@hotmail.com>
2021-06-03 12:06:52 +0200

    [refactor: server folder structure]

--- client/src/views/login/Login.vue
+++ client/src/views/login/Login.vue
@@ -84,6 +84,7 @@ export default {
           }
         })
         .catch((err) => {
+          console.log(err);
           this.errors.push("Er is helaas geen account gevonden");
         });
     },
--- server/app/controllers/user.controller.js
+++ server/app/controllers/user.controller.js
@@ -1,27 +1,119 @@
-// const db = require('../models/index.js');
-// const Users = db.users;
+const {
+  checkValidUser,
+  createUser,
+  findOneUser,
+  getAllUsers,
+} = require('../helper/helpers.js');
 
-// Create and Save a new Tutorial
-exports.create = (req, res) => {};
+// Login handler
+async function handleLogin(req, res) {
+  const user = await checkValidUser(req.body.emailAddress, req.body.password);
 
-// Retrieve all Tutorials from the database.
-exports.findAll = (req, res) => {};
+  console.log(user)
 
-// Find a single Tutorial with an id
-exports.findOne = async(email) => {
-    console.log(email);
-    // const userData = await Users.findOne({ emailAddress: email });
-    // return userData};
+  if (user == null) {
+    return res.sendStatus(400);
+  } else {
+    return res.status(200).json(user);
+  }
+}
+
+// Add user to db
+function handleRegister(req, res) {
+  const userObject = {
+    firstName: req.body.firstName,
+    surName: req.body.surName,
+    emailAddress: req.body.emailAddress,
+    password: req.body.password,
+    birthDate: req.body.birthDate,
+    town: req.body.town,
+    gender: req.body.gender,
+    typeIllness: req.body.typeIllness,
+    profileAvatar: req.body.profileAvatar,
+    about: req.body.about,
+  };
+
+  console.log('User register data: ', userObject);
+  createUser(userObject);
+}
+
+// Get all users
+async function handleUsers(req, res) {
+  // get all users
+  const usersData = await getAllUsers();
+
+  // return data
+  return res.json(usersData);
+}
+
+// Get one specific user by userID
+async function handleUser(req, res) {
+  // get user data
+  const userData = await findOneUser(req.params.id);
+
+  // return data
+  return res.json(userData);
+}
+
+// Get all chats from user by userID
+async function handleChats(req, res) {
+  // get user data
+  const userData = await findOneUser(req.params.id);
+
+  const chatsData = userData.chats;
+  console.log(chatsData);
+
+  // return data
+  return res.json(chatsData);
+}
+
+// Get all participant data per chat
+async function handleChatParticipants(req, res) {
+  // get user data
+  const userData = await findOneUser(req.params.id);
+
+  const userName = {
+    firstName: userData.firstName,
+    surName: userData.surName,
+  };
+
+  // return data
+  return res.json(userName);
+}
+
+module.exports = {
+  handleLogin,
+  handleRegister,
+  handleUsers,
+  handleUser,
+  handleChats,
+  handleChatParticipants,
 };
 
-// Update a Tutorial by the id in the request
-exports.update = (req, res) => {};
+// // const db = require('../models/index.js');
+// // const Users = db.users;
 
-// Delete a Tutorial with the specified id in the request
-exports.delete = (req, res) => {};
+// // Create and Save a new Tutorial
+// exports.create = (req, res) => {};
 
-// Delete all Tutorials from the database.
-exports.deleteAll = (req, res) => {};
+// // Retrieve all Tutorials from the database.
+// exports.findAll = (req, res) => {};
 
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {};
+// // Find a single Tutorial with an id
+// exports.findOne = async(email) => {
+//     console.log(email);
+//     // const userData = await Users.findOne({ emailAddress: email });
+//     // return userData};
+// };
+
+// // Update a Tutorial by the id in the request
+// exports.update = (req, res) => {};
+
+// // Delete a Tutorial with the specified id in the request
+// exports.delete = (req, res) => {};
+
+// // Delete all Tutorials from the database.
+// exports.deleteAll = (req, res) => {};
+
+// // Find all published Tutorials
+// exports.findAllPublished = (req, res) => {};
