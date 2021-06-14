1e60639134d0cca2fc8171c81409bb9254758050
PC-ROWIN\rowin <rowin_ruizendaal@hotmail.com>
2021-06-14 11:16:39 +0200

    [refactor: remove ununsed code]

--- client/src/views/themes/theme.vue
+++ client/src/views/themes/theme.vue
@@ -37,34 +37,6 @@ export default {
   components: {
     Nav,
   },
-  data() {
-    return {
-      steps: [
-        {
-          target: "#v-step-0", // We're using document.querySelector() under the hood
-          header: {
-            title: "Get Started",
-          },
-          content: `Discover <strong>Vue Tour</strong>!`,
-        },
-        {
-          target: ".v-step-1",
-          content: "An awesome plugin made with Vue.js!",
-        },
-        {
-          target: '[data-v-step="2"]',
-          content:
-            "Try it, you'll love it!<br>You can put HTML in the steps and completely customize the DOM to suit your needs.",
-          params: {
-            placement: "top", // Any valid Popper.js placement. See https://popper.js.org/popper-documentation.html#Popper.placements
-          },
-        },
-      ],
-    };
-  },
-  mounted: function () {
-    this.$tours["myTour"].start();
-  },
   computed: {
     themelist() {
       return this.$store.getters.themelist;
